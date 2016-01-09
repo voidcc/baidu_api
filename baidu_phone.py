3495e8ec48d450fbec6d4f807228763b52dad4d9
voidcc <voidcc@sina.com>
2016-01-09 11:46:20 +0800

    add phone & weather

---
+++ baidu_phone.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import sys, urllib, urllib2, json
+
+
+def get_apikey():
+    f = open('apikey.txt','r')
+    apikey = f.readline()
+    f.close()
+    return apikey
+
+def generate_url():
+    url = 'http://apis.baidu.com/apistore/mobilenumber/mobilenumber?phone='
+    phone_num = raw_input('Please input your phone number --> ')
+    url += phone_num
+    return url
+
+def request_baidu(url, apikey):
+    headers = {'apikey': apikey}
+    req = urllib2.Request(url, headers = headers)
+    resp = urllib2.urlopen(req)
+    if resp.getcode() == 200:
+        content = resp.read()
+        #content = content.encode('utf-8')
+        if(content):
+            print(json.loads(content))
+
+if __name__ == "__main__":
+    apikey = get_apikey()
+    url = generate_url()
+    request_baidu(url, apikey)
+    
