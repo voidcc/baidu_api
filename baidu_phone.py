27fd415af44e7c6ac81177c655b7f85932bb3f42
voidcc <voidcc@sina.com>
2016-01-09 22:18:39 +0800

    optimize the output

--- baidu_phone.py
+++ baidu_phone.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys, urllib, urllib2, json
 
-
 def get_apikey():
     f = open('apikey.txt','r')
     apikey = f.readline()
@@ -9,9 +8,10 @@ def get_apikey():
     return apikey
 
 def generate_url():
-    url = 'http://apis.baidu.com/apistore/mobilenumber/mobilenumber?phone='
+    url = 'http://apis.baidu.com/apistore/mobilenumber/mobilenumber?'
     phone_num = raw_input('Please input your phone number --> ')
-    url += phone_num
+    params = {'phone': phone_num}
+    url += urllib.urlencode(params)
     return url
 
 def request_baidu(url, apikey):
@@ -20,9 +20,9 @@ def request_baidu(url, apikey):
     resp = urllib2.urlopen(req)
     if resp.getcode() == 200:
         content = resp.read()
-        #content = content.encode('utf-8')
-        if(content):
-            print(json.loads(content))
+        content = json.loads(content)
+        for k,v in content['retData'].iteritems():
+            print k, v
 
 if __name__ == "__main__":
     apikey = get_apikey()
--- baidu_weather.py
+++ baidu_weather.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys, urllib, urllib2, json
 
-
 def get_apikey():
     f = open('apikey.txt','r')
     apikey = f.readline()
@@ -9,22 +8,35 @@ def get_apikey():
     return apikey
 
 def generate_url():
-    url = 'http://apis.baidu.com/apistore/weatherservice/citylist?'
-    #phone_num = raw_input('Please input your phone number --> ')
-    params = {'cityname': '朝阳'}
+    url = 'http://apis.baidu.com/heweather/weather/free?'
+    params = {'city': 'hefei'}
     url += urllib.urlencode(params)
-    print url
     return url
 
 def request_baidu(url, apikey):
     headers = {'apikey': apikey}
-    req = urllib2.Request(url, headers = headers)
-    resp = urllib2.urlopen(req)
-    if resp.getcode() == 200:
+    try:
+        req = urllib2.Request(url, headers = headers)
+        resp = urllib2.urlopen(req)
+    except urllib2.HTTPError, e:
+        print(e.code)
+    except urllib2.URLError, e:
+        print(e.reason)
+    else:
         content = resp.read()
-        #content = content.encode('utf-8')
-        if(content):
-            print(json.loads(content))
+        content = json.loads(content)
+        #print content.keys()
+        content = content.get("HeWeather data service 3.0")[0]
+        if content is not None and content.get('status') == 'ok':
+            print content['status']
+        """
+        if content.get('errNum') == 0:
+            for item in content['retData']:
+                for k,v in item.iteritems():
+                    print k, v
+        """
+
+
 
 if __name__ == "__main__":
     apikey = get_apikey()
